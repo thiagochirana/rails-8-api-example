fc5e3cc64f2cc98dcc1055510aec36fa2f69cf2e
Chirana <engsw.thiago@gmail.com>
2025-02-25 00:45:41 -0300

    impr: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_25_034355) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_15_025903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "product_variants", force: :cascade do |t|
-    t.string "size"
-    t.string "color"
-    t.string "estampa"
-    t.string "material_type"
-    t.string "capacity_ml"
-    t.string "weight_g"
-    t.string "stock_quantity"
-    t.bigint "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_product_variants_on_product_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.bigint "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -51,16 +21,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_25_034355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
-  create_table "stock_transactions", force: :cascade do |t|
-    t.bigint "product_variant_id", null: false
-    t.string "quantity"
-    t.string "type"
-    t.float "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_variant_id"], name: "index_stock_transactions_on_product_variant_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,8 +37,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_25_034355) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "product_variants", "products"
-  add_foreign_key "products", "categories"
   add_foreign_key "sessions", "users"
-  add_foreign_key "stock_transactions", "product_variants"
 end
